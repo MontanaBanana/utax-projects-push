09731eed675b1bb4441987058b1c7b82c7838bc5
www-data <www-data@taxmobileapp.com>
2020-01-16 18:21:38 +0000

    Template updated

--- src/pages/agreement/agreement.ts
+++ src/pages/agreement/agreement.ts
@@ -13,7 +13,7 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'page-agreement',
-  templateUrl: '{hostname}/apptemplate/agreement/{project.id}'
+  templateUrl: 'https://taxmobileapp.com/apptemplate/agreement/381'
 })
 export class AgreementPage {
 
