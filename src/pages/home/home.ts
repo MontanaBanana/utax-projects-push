16bc287d49f1ee4eb92e943e7976a65975f339d9
www-data <www-data@taxmobileapp.com>
2020-04-17 17:25:16 +0000

    Template updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,7 +21,7 @@ import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'page-home',
-  templateUrl: '{hostname}/apptemplate/home/{project.id}'
+  templateUrl: 'https://taxmobileapp.com/apptemplate/home/525'
 })
 export class HomePage {
   public src;
